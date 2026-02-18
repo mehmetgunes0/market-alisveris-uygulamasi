068a0ffe2775a629e4d2ca1880548f492f091e47
mehmetgunes0 <mehmettgunes7@gmail.com>
2026-02-18 21:20:17 +0300

    market_alisverisi_uygulama.py

--- market_alisverisi_uygulama.py
+++ market_alisverisi_uygulama.py
@@ -1,19 +1,3 @@
-"""
-Kullanıcıdan ürün ve fiyat bilgilerini alarak bir alışveriş sepeti oluşturmak, ardından bakiye kontrolü yapıp, yeterli değilse sepetten ürün silmek.
-
-Görev Tanımı:
-1. Ürün ve fiyat bilgilerini kullanıcıdan alın (örneğin: “Ekmek – 10 TL”, “Süt – 25 TL”).
-• Kullanıcı “X” yazana kadar ürün ve fiyat almaya devam edin.
-• Ürün adlarını urunler listesinde, fiyatları fiyatlar listesinde saklayın.
-2. Kullanıcıdan bakiye bilgisini isteyin.
-3. for döngüsüyle sepeti ekrana yazdırın ve toplam fiyatı hesaplayın.
-4. if yapısı ile kontrol edin:
-• Eğer bakiye yeterliyse
-• Eğer bakiye yetmiyorsa:
-5. Bakiyesi yetmeyen kullanıcıya while döngüsüyle ürün silme seçeneği verin.
-• Ürün silindikçe toplam fiyatı yeniden hesaplayın.
-• Bakiye yeterli olunca siparişi onaylayın.
-"""
 
 ürün = []
 fiyat = []
